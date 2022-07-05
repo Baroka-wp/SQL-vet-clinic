6c7e6f10dc60f1e1f220bd0fb87ef4182ee96fab
baroka <birotori@gmail.com>
2022-07-05 22:45:13 +0100

    Change query and update animals table

--- data.sql
+++ data.sql
@@ -40,7 +40,7 @@ INSERT INTO animals (
         neutered,
         escape_attempts
     )
-VALUES('Charmander', '2020-02-08', 11, FALSE, 0);
+VALUES('Charmander', '2020-02-08', -11, FALSE, 0);
 INSERT INTO animals (
         name,
         date_of_birth,
@@ -48,7 +48,7 @@ INSERT INTO animals (
         neutered,
         escape_attempts
     )
-VALUES('Plantmon', '2021-11-15', 5.7, TRUE, 2);
+VALUES('Plantmon', '2022-11-15', -5.7, TRUE, 2);
 INSERT INTO animals (
         name,
         date_of_birth,
@@ -56,7 +56,7 @@ INSERT INTO animals (
         neutered,
         escape_attempts
     )
-VALUES('Squirtle', '1993-04-02', 12.13, FALSE, 3);
+VALUES('Squirtle', '1993-04-02', -12.13, FALSE, 3);
 INSERT INTO animals (
         name,
         date_of_birth,
@@ -64,7 +64,7 @@ INSERT INTO animals (
         neutered,
         escape_attempts
     )
-VALUES('Angemon', '2005-06-12', 45, TRUE, 1);
+VALUES('Angemon', '2005-06-12', -45, TRUE, 1);
 INSERT INTO animals (
         name,
         date_of_birth,
@@ -90,47 +90,3 @@ INSERT INTO animals (
     )
 VALUES('Ditto', '2022-05-14', 22, TRUE, 4);
 COMMIT;
--- update the animals table by setting the species column to unspecified
-BEGIN;
-UPDATE animals
-SET species = 'unspecified';
-ROLLBACK;
-/* Update the animals table by setting the species column to digimon 
- for all animals that have a name ending in mon*/
-BEGIN;
-UPDATE animals
-SET species = 'digimon'
-WHERE name LIKE '%mon';
-/* Update the animals table by setting the species column 
- to pokemon for all animals that don't have species already set.*/
-UPDATE animals
-SET species = 'pokemon'
-WHERE species IS NULL;
--- commit the transaction 
-COMMIT;
--- Verify changes
-BEGIN;
--- delete all records in the animals table
-DELETE FROM animals;
--- rollback the transaction
-ROLLBACK;
----------------------------------
-BEGIN;
--- Delete all animals born after Jan 1st, 2022.
-DELETE FROM animals
-WHERE date_of_birth > '2022-01-01';
--- Create a savepoint for the transaction.
-SAVEPOINT SP1;
--- Update all animals' weight to be their weight multiplied by -1.
-UPDATE animals
-SET weight_kg = weight_kg * -1;
--- Rollback to the savepoint
-ROLLBACK TO SP1;
--- Update all animals' weights that are negative to be their weight multiplied by -1
-UPDATE animals
-SET weight_kg = weight_kg * -1
-WHERE weight_kg < 0;
--- Commit transaction
-COMMIT;
-SELECT *
-FROM animals;
--- queries.sql
+++ queries.sql
@@ -26,6 +26,70 @@ SELECT *
 FROM animals
 WHERE weight_kg >= 10.4
     AND weight_kg <= 17.3;
+-- update the animals table by setting the species column to unspecified
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT species
+FROM animals;
+ROLLBACK;
+SELECT species
+FROM animals;
+/* Update the animals table by setting the species column to digimon 
+ for all animals that have a name ending in mon*/
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+/* Update the animals table by setting the species column 
+ to pokemon for all animals that don't have species already set.*/
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+SELECT species
+FROM animals;
+-- commit the transaction 
+COMMIT;
+SELECT species
+FROM animals;
+-- Verify changes
+BEGIN;
+-- delete all records in the animals table
+DELETE FROM animals;
+SELECT COUNT(*)
+FROM ANIMALS;
+-- rollback the transaction
+ROLLBACK;
+SELECT COUNT(*)
+FROM ANIMALS;
+---------------------------------
+BEGIN;
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+-- Create a savepoint for the transaction.
+SAVEPOINT SP1;
+SELECT COUNT(*)
+FROM animals;
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals
+SET weight_kg = -1 * weight_kg;
+SELECT weight_kg
+FROM animals;
+-- Rollback to the savepoint
+ROLLBACK TO SP1;
+SELECT weight_kg
+FROM animals;
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+SELECT weight_kg
+FROM animals;
+-- Commit transaction
+COMMIT;
+SELECT weight_kg
+FROM animals;
 -- Aggrgation ------
 -- How many animals are there?
 SELECT COUNT(*)
@@ -41,18 +105,22 @@ SELECT AVG(weight_kg)
 FROM animals;
 -- result : 15.5500
 -- Who escapes the most, neutered or not neutered animals?
-SELECT MAX(escape_attempts)
+SELECT neutered,
+    MAX(escape_attempts)
 FROM animals
 GROUP BY neutered;
 -- result : 7 | 5
 -- What is the minimum and maximum weight of each type of animal?
-SELECT MAX(weight_kg),
+SELECT species,
+    MAX(weight_kg),
     MIN(weight_kg)
-FROM animals;
+FROM animals
+GROUP BY species;
 -- result : MIN = 5.7 | MAX = 45
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT AVG(escape_attempts)
+SELECT species,
+    AVG(escape_attempts)
 FROM animals
-WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
 -- result : 3.000
