7a9008586119410c3d89f26efc36d4953a327662
baroka <birotori@gmail.com>
2022-07-06 20:00:19 +0100

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,26 @@ CREATE TABLE animals (
 -- Add a column species of type string to your animals table.
 ALTER TABLE animals
 ADD species VARCHAR(100);
+-- Create a table named owners with the following columns
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100)
+);
+--Create a table named species with the following columns
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INT
+);
+-- Remove column species
+ALTER TABLE animals DROP COLUMN species;
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD species_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_species_table FOREIGN KEY(specices_id) REFERENCES species(id);
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD owner_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner_table FOREIGN KEY(owner_id) REFERENCES owners(id);
